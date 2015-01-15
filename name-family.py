9a3b030c8c448c5a230bea0a0a869e75c8960e32
Mark Galloway <mgallowa@cs.ualberta.ca>
2015-01-14 17:45:59 -0700

    fixes

--- name-family.py
+++ name-family.py
@@ -1,5 +1,5 @@
 class Student():
-  """docstring for Student"""
+  """A Student"""
   def __init__(self, name, family):
     self.name = name
     self.family = family
@@ -9,11 +9,21 @@ class Student():
     self.courseMarks[course] = mark
   
   def average(self):
-    return sum(self.courseMarks.values()) / len(self.courseMarks)
+    try:
+      return sum(self.courseMarks.values()) / len(self.courseMarks) 
+    except ZeroDivisionError:
+      print("Student %s has no courses." % self.name)
 
-# Test
+
+# Test 1
 student = Student("Mark", "Galloway")
 student.addCourse("Math", 100)
 student.addCourse("C410", 90)
 student.addCourse("English 101", 80)
 assert(student.average() == 90)
+
+# Test 2
+studentB = Student("Fred", "Flintstone")
+studentB.average()  # Test no courses (visual print out)
+studentB.addCourse("Dino101", 51)
+assert(studentB.average() == 51)
